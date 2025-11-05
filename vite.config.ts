0579127c79069c8c1d2d654b38c767f125727236
Zeidan chabo <zeidanchabo@gmail.com>
2025-11-04 22:58:09 -0500

    builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwind from "@tailwindcss/vite"
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
-    tailwind()
+    tailwind(),
   ],
   resolve: {
-    alias:{
-      "@":path.resolve(__dirname,"./src")
-    }
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
